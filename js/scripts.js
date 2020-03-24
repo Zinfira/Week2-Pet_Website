d0c02a8766cf9bc3dd07502aa2d1e4d4f6fa9226
William Pratt <willpratt78@gmail.com>
2020-03-24 12:02:29 -0700

    add button options in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
-  $("button#dark").click(function() {
+   $("button#dark").click(function() {
     $("body").removeClass();
     $("body").addClass("change-mode");
+    $("dark").fadeIn();
+    $("light").hide();
 
-  });
+  // }) else ({
+
+    $("button#light").click(function() {
+      $("change-mode").removeClass();
+      $("change-mode").addClass("body");
+      })
+    });
 });
